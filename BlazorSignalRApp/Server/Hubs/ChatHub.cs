66fb17c236438851f80308b3b99c790d202cf011
Wael Kdouh <waekdo@microsoft.com>
2020-10-28 22:23:21 -0400

    initiall commit

---
+++ BlazorSignalRApp/Server/Hubs/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorSignalRApp.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message, bool approved)
+        {
+            if (approved)
+            {
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, "refuse");
+                await Groups.AddToGroupAsync(Context.ConnectionId, "approved");
+                await Clients.Group("approved").SendAsync("ReceiveMessage", user, message, approved);
+            }
+            else
+            {
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, "approved");
+                await Groups.AddToGroupAsync(Context.ConnectionId, "refuse");
+                await Clients.Group("refuse").SendAsync("ReceiveMessage", user, message, approved);
+            }
+        }
+    }
+}
